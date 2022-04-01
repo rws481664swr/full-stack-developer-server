b6b4cc9639d8a2b97bb240ca15a16206e61a777f
sam raphael <samraphael@gmail.com>
2022-04-01 10:23:15 -0400

    first setup commit

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import userController   from "./controllers/user-controller.js";
+import helloController  from "./controllers/hello-controller.js";
+import tuitsController   from "./controllers/tuits-controller.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+helloController(app);
+userController(app);
+tuitsController(app);
+
+app.get('/hello', (req, res) => {res.send('Hello World!')})
+
+app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+
+
+app.listen(4000);
+
+
